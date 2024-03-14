422732cf4d12c61f65e3bf063ee729b1a3ca1151
Andres Lora <jalc.andres.lora@gmail.com>
2024-03-14 13:57:20 -0700

    Implementing navigation with signIn, signOut, home, and logout

--- src/App.js
+++ src/App.js
@@ -7,11 +7,24 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Logo from './components/Logo/Logo';
 import Navigation from './components/Navigation/Navigation';
 import Rank from './components/Rank/Rank';
+import Register from './components/Register/Register';
+import SignIn from './components/SignIn/SignIn';
 
 function App() {
+  const [route, setRoute] = useState('signIn');
+  const [isSignedIn, setIsSignedIn] = useState(false);
   const [input, setInput] = useState('');
   const [imageUrl, setImageUrl] = useState('');
   const [boxes, setBoxes] = useState([]);
+  const [user, setUser] = useState({
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: '',
+    },
+  });
 
   const onInputChange = (event) => {
     setInput(event.target.value);
@@ -34,6 +47,28 @@ function App() {
     return boxes;
   };
 
+  const onRouteChange = (route) => {
+    console.log(route);
+    if (route === 'signOut') {
+      setIsSignedIn(false);
+    } else if (route === 'home') {
+      setIsSignedIn(true);
+    }
+    setRoute(route);
+  };
+
+  const loadUser = (data) => {
+    setUser({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
   const displayFaceBoxes = (boxes) => {
     setBoxes(boxes);
   };
@@ -47,14 +82,22 @@ function App() {
   return (
     <div className="App">
       <ParticlesBg color="#ff0000" num={200} type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm
-        onInputChange={onInputChange}
-        onButtonSubmit={onButtonSubmit}
-      />
-      <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={onRouteChange} />
+      {route === 'home' ? (
+        <div>
+          <Logo />
+          <Rank name={user.name} entries={user.entries} />
+          <ImageLinkForm
+            onInputChange={onInputChange}
+            onButtonSubmit={onButtonSubmit}
+          />
+          <FaceRecognition boxes={boxes} imageUrl={imageUrl} />
+        </div>
+      ) : route === 'signIn' ? (
+        <SignIn loadUser={loadUser} onRouteChange={onRouteChange} />
+      ) : (
+        <Register loadUser={loadUser} onRouteChange={onRouteChange} />
+      )}
     </div>
   );
 }
