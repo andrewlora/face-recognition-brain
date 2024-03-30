142ee808d8e95c11cebd26e50da65a621c50a653
Andres Lora <jalc.andres.lora@gmail.com>
2024-03-30 16:01:12 -0700

    Implementing Knex.js with Postgres

--- server/server.js
+++ server/server.js
@@ -1,82 +1,91 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config({ path: ['.env.local', '.env'] });
+console.log(process.env);
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cors());
-const PORT = 3001;
-const dataBase = {
-  users: [
-    {
-      id: '123',
-      name: 'john',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '1234',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [{}],
+var corsOptions = {
+  origin: 'http://localhost:' + process.env.NODE_DOCKER_PORT,
 };
+
+app.use(cors(corsOptions));
+app.use(cors());
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
+
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+
 app.get('/', (req, res) => {
-  res.send(dataBase.users);
+  knex
+    .select('*')
+    .from('users')
+    .then((data) => {
+      console.log('database info:', data);
+      res.send(data);
+    });
 });
 app.post('/signIn', (req, res) => {
-  if (
-    req.body.email === dataBase.users[0].email &&
-    req.body.password === dataBase.users[0].password
-  ) {
-    res.json(dataBase.users[0]);
-  } else {
-    res.status(400).json('Error logging in');
-  }
+  res.status(400).json('Error logging in');
 });
 
 app.post('/register', (req, res) => {
-  const { email, name, password } = req.body;
-  const newUser = {
-    id: '12345',
-    name: name,
-    email: email,
-    password: password,
-    entries: 0,
-    joined: new Date(),
-  };
-  dataBase.users.push(newUser);
-  res.json(newUser);
+  const { email, name } = req.body;
+  knex('users')
+    .returning('*')
+    .insert({
+      name: name,
+      email: email,
+      joined: new Date(),
+    })
+    .then((user) => {
+      res.json(user[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json('Unable to register');
+    });
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  dataBase.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) res.status(404).json('not found user');
+  console.log('/profile/', id);
+  knex('users')
+    .where('id', id)
+    .then((user) => {
+      if (user?.length) {
+        res.json(user[0]);
+      }
+      res.status(404).json('not found user');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json('Error getting user');
+    });
 });
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  dataBase.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user);
-    }
-  });
-  if (!found) res.status(404).json('not found user');
+  knex('users')
+    .where('id', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then((entries) => {
+      console.log(entries);
+      res.json(entries[0]);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json('Unable to get entries');
+    });
 });
 
 app.listen(PORT, () => {
