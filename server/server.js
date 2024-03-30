19ddcacb4ec7d80d5d52e36b6c287029dd4435fc
Andres Lora <jalc.andres.lora@gmail.com>
2024-03-30 16:46:57 -0700

    Implemented bcrypt inside register and login functions with hash/salt

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bcrypt = require('bcrypt');
 require('dotenv').config({ path: ['.env.local', '.env'] });
 console.log(process.env);
 app.use(express.json()); // for parsing application/json
@@ -34,20 +35,62 @@ app.get('/', (req, res) => {
     });
 });
 app.post('/signIn', (req, res) => {
-  res.status(400).json('Error logging in');
+  const { email, password } = req.body;
+  knex
+    .select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash);
+      if (isValid) {
+        return knex
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(400).json('unable to get user');
+          });
+      } else {
+        res.status(400).json('Wrong credentials');
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json('Error logging in');
+    });
 });
 
 app.post('/register', (req, res) => {
-  const { email, name } = req.body;
-  knex('users')
-    .returning('*')
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
+  const { email, name, password } = req.body;
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+  knex
+    .transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into('login')
+        .returning('email')
+        .then((result) => {
+          return trx('users')
+            .returning('*')
+            .insert({
+              name: name,
+              email: result[0].email,
+              joined: new Date(),
+            })
+            .then((user) => {
+              res.json(user[0]);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
     })
     .catch((err) => {
       console.log(err);
